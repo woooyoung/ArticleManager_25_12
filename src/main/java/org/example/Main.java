37afcfc61ea12732468b4a670ea09109bb72fb31
woooyoung <axdsw121@gmail.com>
2025-12-08 14:58:43 +0900

    날짜 기능 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,6 +15,8 @@ public class Main {
 
         List<Article> articles = new ArrayList<>();
 
+//        makeTestData();
+
         while (true) {
             System.out.print("명령어 ) ");
             String cmd = sc.nextLine().trim();
@@ -33,8 +35,10 @@ public class Main {
                 String title = sc.nextLine().trim();
                 System.out.print("내용 : ");
                 String body = sc.nextLine().trim();
+                String regDate = Util.getNowStr();
+                String updateDate = Util.getNowStr();
 
-                Article article = new Article(id, title, body);
+                Article article = new Article(id, regDate, updateDate, title, body);
                 articles.add(article);
 
                 System.out.println(id + "번 글이 작성되었습니다.");
@@ -44,10 +48,15 @@ public class Main {
                 if (articles.size() == 0) {
                     System.out.println("아무것도 없음");
                 } else {
-                    System.out.println("   번호  /     제목     /   내용  ");
+                    System.out.println("   번호  /       날짜       /       제목     /   내용  ");
                     for (int i = articles.size() - 1; i >= 0; i--) {
                         Article article = articles.get(i);
-                        System.out.printf("   %d    /    %s     /     %s   \n", article.getId(), article.getTitle(), article.getBody());
+                        if(Util.getNowStr().split(" ")[0].equals(article.getRegDate().split(" ")[0])) {
+                            System.out.printf("   %d     /    %s          /    %s     /     %s   \n", article.getId(), article.getRegDate().split(" ")[1], article.getTitle(), article.getBody());
+                        }else {
+                            System.out.printf("   %d     /    %s          /    %s     /     %s   \n", article.getId(), article.getRegDate().split(" ")[0], article.getTitle(), article.getBody());
+                        }
+
                     }
                 }
             } else if (cmd.startsWith("article detail")) {
@@ -69,6 +78,8 @@ public class Main {
                     continue;
                 }
                 System.out.println("번호 : " + foundArticle.getId());
+                System.out.println("작성날짜 : " + foundArticle.getRegDate());
+                System.out.println("수정날짜 : " + foundArticle.getUpdateDate());
                 System.out.println("제목 : " + foundArticle.getTitle());
                 System.out.println("내용 : " + foundArticle.getBody());
 
@@ -119,6 +130,9 @@ public class Main {
 
                 foundArticle.setTitle(newTitle);
                 foundArticle.setBody(newBody);
+
+                foundArticle.setUpdateDate(Util.getNowStr());
+
                 System.out.println(id + "번 게시글이 수정되었습니다");
             } else {
                 System.out.println("사용할 수 없는 명령어입니다");
@@ -131,11 +145,37 @@ public class Main {
 
 class Article {
     private int id;
+    private String regDate;
+    private String updateDate;
     private String title;
     private String body;
 
+    public Article(int id, String regDate, String updateDate, String title, String body) {
+        this.id = id;
+        this.regDate = regDate;
+        this.updateDate = updateDate;
+        this.title = title;
+        this.body = body;
+    }
+
     public int getId() {
         return id;
+    }
+
+    public String getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
     }
 
     public void setId(int id) {
@@ -158,9 +198,5 @@ class Article {
         this.body = body;
     }
 
-    public Article(int id, String title, String body) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-    }
+
 }
